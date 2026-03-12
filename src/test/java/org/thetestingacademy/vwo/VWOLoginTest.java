d6687c44f775f2d78ed475f91890b1585e5fb430
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-12 21:59:04 +0530

    VWOLogin Test

--- src/test/java/org/thetestingacademy/vwo/VWOLoginTest.java
+++ src/test/java/org/thetestingacademy/vwo/VWOLoginTest.java
@@ -20,25 +20,19 @@ public class VWOLoginTest extends BaseTest {
     By password = By.cssSelector("#login-password");
     By signBtn = By.xpath("//button[@id = 'js-login-btn']");
     By errorMsgNotification = By.cssSelector("#js-notification-box-msg");
+
+    WebElement loginIDElement;
+
     @Test
-    public void doLogin()
-    {
+    public void doLogin() {
         driver.get("https://app.vwo.com/#/login");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-         //CommonToAll.getElementByCssSelector(driver, loginID );
-      //  WaitHelper.waitForVisibilityOfElementLocated(driver, loginID,Duration.ofSeconds(2));
-        CommonToAll.getElementByCssSelector(driver, loginID ).sendKeys("admin@admin.com");
 
-      //  WaitHelper.waitForVisibilityOfElementLocated(driver, password,Duration.ofSeconds(2));
-        CommonToAll.getElementByCssSelector(driver, password ).sendKeys("admin");
+        WaitHelper.waitForVisibilityOfElementLocated(driver, loginID, Duration.ofSeconds(2)).sendKeys("admin@admin.com");
+        WaitHelper.waitForVisibilityOfElementLocated(driver, password, Duration.ofSeconds(2)).sendKeys("admin");
+        WaitHelper.waitForVisibilityOfElementLocated(driver, signBtn, Duration.ofSeconds(2)).click();
+        WaitHelper.waitForVisibilityOfElementLocated(driver, errorMsgNotification, Duration.ofSeconds(2));
 
-      //  WaitHelper.waitForVisibilityOfElementLocated(driver, signBtn , Duration.ofSeconds(2));
-        CommonToAll.getElementByXpath(driver, signBtn ).click();
-
-        WaitHelper.waitForVisibilityOfElementLocated(driver, errorMsgNotification,Duration.ofSeconds(2));
-        String errorMsg = CommonToAll.getElementByCssSelector(driver, errorMsgNotification ).getText();
-
-
-        Assert.assertEquals(errorMsg,"Your email, password, IP address or location did not match");
+        String errorMsg = CommonToAll.getElementByCssSelector(driver, errorMsgNotification).getText();
+        Assert.assertEquals(errorMsg, "Your email, password, IP address or location did not match");
     }
 }
