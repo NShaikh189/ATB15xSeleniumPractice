2d791df53eaa06ccf767c99a2708a3668bf42a44
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-15 00:35:21 +0530

    Wait for document readystate is complete

--- src/test/java/org/thetestingacademy/Utility/WaitHelper.java
+++ src/test/java/org/thetestingacademy/Utility/WaitHelper.java
@@ -1,8 +1,6 @@
 package org.thetestingacademy.Utility;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,39 +8,45 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class WaitHelper {
-    public static WebDriverWait wait;
+
 
     private WaitHelper()
     {}
+    public static boolean waitForPageLoad(WebDriver driver, Duration timeInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds) ;
+        return wait.until(d-> (JavascriptExecutor)d)
+                .executeScript("return document.readyState")
+                .equals("complete");
+        //return Boolean.parseBoolean(wait.until(ExpectedConditions.jsReturnsValue("return document.readyState==='complete'")).toString());
+    }
     public static WebElement waitForVisibilityOfElement(WebDriver driver, WebElement element, Duration timeInSeconds)
     {
-        wait = new WebDriverWait(driver, timeInSeconds);
+        WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static WebElement waitForVisibilityOfElementLocated(WebDriver driver, By locator, Duration timeInSeconds)
     {
-        wait = new WebDriverWait(driver, timeInSeconds);
+        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public static boolean waitForElementToBeSelected(WebDriver driver, By locator, Duration timeInSeconds)
     {
-        wait = new WebDriverWait(driver, timeInSeconds);
-        WebElement element = waitForVisibilityOfElementLocated(driver,locator,timeInSeconds);
-      return  wait.until(ExpectedConditions.elementToBeSelected(element));
+        WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+      return  wait.until(ExpectedConditions.elementToBeSelected(locator));
 
     }
     public static WebElement waitForElementToBeClickable(WebDriver driver, By locator, Duration timeInSeconds)
     {
-        wait = new WebDriverWait(driver, timeInSeconds);
-        WebElement element = waitForVisibilityOfElementLocated(driver,locator,timeInSeconds);
-        return wait.until(ExpectedConditions.elementToBeClickable(element));
+        WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
-    public static void waitForAlertIsPresent(WebDriver driver, Duration timeInSeconds)
+    public static Alert waitForAlertIsPresent(WebDriver driver, Duration timeInSeconds)
     {
-        wait = new WebDriverWait(driver, timeInSeconds);
-        wait.until(ExpectedConditions.alertIsPresent());
+        WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+       return wait.until(ExpectedConditions.alertIsPresent());
 
     }
 }
