9d4910d0c59dd72c9c3492fb46ea15fa9b086ad0
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-18 06:39:11 +0530

    Added a static final value for Default duration of seconds and Overloaded methods

--- src/test/java/org/thetestingacademy/Utility/WaitHelper.java
+++ src/test/java/org/thetestingacademy/Utility/WaitHelper.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class WaitHelper {
 
-
+    static final Duration DEFAULT_TIME_IN_SECONDS = Duration.ofSeconds(5);
     private WaitHelper()
     {}
 
@@ -30,10 +30,21 @@ public class WaitHelper {
         WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
-
+    public static WebElement waitForVisibilityOfElement(WebDriver driver, WebElement element)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
     public static List<WebElement> waitForVisibilityOfAllElements(WebDriver driver, By locator, Duration timeInSeconds)
     {
         WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+        return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    }
+
+
+    public static List<WebElement> waitForVisibilityOfAllElements(WebDriver driver, By locator)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
         return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
     }
 
@@ -43,10 +54,34 @@ public class WaitHelper {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
+    public static WebElement waitForVisibilityOfElementLocated(WebDriver driver, By locator)
+    {
+        WebDriverWait wait = new WebDriverWait(driver,DEFAULT_TIME_IN_SECONDS);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
     public static boolean waitForElementToBeSelected(WebDriver driver, By locator, Duration timeInSeconds)
     {
         WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
       return  wait.until(ExpectedConditions.elementToBeSelected(locator));
+
+    }
+    public static boolean waitForElementToBeSelected(WebDriver driver, By locator)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
+        return  wait.until(ExpectedConditions.elementToBeSelected(locator));
+
+    }
+    public static boolean waitForElementToBeSelected(WebDriver driver, WebElement element, Duration timeInSeconds)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+        return  wait.until(ExpectedConditions.elementToBeSelected(element));
+
+    }
+    public static boolean waitForElementToBeSelected(WebDriver driver, WebElement element)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
+        return  wait.until(ExpectedConditions.elementToBeSelected(element));
 
     }
     public static WebElement waitForElementToBeClickable(WebDriver driver, By locator, Duration timeInSeconds)
@@ -58,6 +93,17 @@ public class WaitHelper {
     public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element, Duration timeInSeconds)
     {
         WebDriverWait  wait = new WebDriverWait(driver, timeInSeconds);
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public static WebElement waitForElementToBeClickable(WebDriver driver, By locator)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element)
+    {
+        WebDriverWait  wait = new WebDriverWait(driver, DEFAULT_TIME_IN_SECONDS);
         return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
     public static Alert waitForAlertIsPresent(WebDriver driver, Duration timeInSeconds)
