cdfe6bdaa8f401953d6ba93f5da58542d6e45092
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-18 06:38:02 +0530

    Keys.chord code upate

--- src/test/java/org/thetestingacademy/GoogleHomePageTest.java
+++ src/test/java/org/thetestingacademy/GoogleHomePageTest.java
@@ -1,5 +1,8 @@
 package org.thetestingacademy;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestNGListener;
 import org.testng.annotations.Test;
 import org.thetestingacademy.Screenshots.TakeSceenshotHelper;
@@ -13,6 +16,8 @@ public class GoogleHomePageTest extends BaseTest implements ITestNGListener{
         driver.get("https://google.com");
 
         TakeSceenshotHelper.captureScreenshot(driver, "googleLandingPage");
+        Actions action = new Actions(driver);
 
+//        action.moveToElement(driver.findElement(By.id("abc"))).sendKeys(Keys.chord(Keys.CONTROL,Keys.ENTER)).build().perform();
     }
 }
