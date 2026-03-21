26693693858c1c2dac27bffcae2fa689f3e89ef3
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-21 23:25:32 +0530

    code update

--- src/test/java/org/thetestingacademy/google/GoogleHomePageTest.java
+++ src/test/java/org/thetestingacademy/google/GoogleHomePageTest.java
@@ -1,20 +1,22 @@
 package org.thetestingacademy.google;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestNGListener;
 import org.testng.annotations.Test;
 import org.thetestingacademy.Factory.DriverFactory;
 import org.thetestingacademy.Screenshots.TakeSceenshotHelper;
-import org.thetestingacademy.Utility.BaseTest;
+import org.thetestingacademy.Base.BaseTest;
 import org.thetestingacademy.logger.Log;
+import org.thetestingacademy.utils.PropertiesReader;
+
+import java.io.IOException;
 
 public class GoogleHomePageTest extends BaseTest implements ITestNGListener{
 
 
     @Test
-    public void launchGooglePageTest()
-    {
+    public void launchGooglePageTest() throws IOException {
+
 
         driver.get("https://google.com");
 
@@ -22,6 +24,8 @@ public class GoogleHomePageTest extends BaseTest implements ITestNGListener{
         Actions action = new Actions(driver);
         Log.info("Google Page loaded successfully");
         Log.info("Screenshot available: "+path);
+
+        PropertiesReader.readKey("username");
 //        action.moveToElement(driver.findElement(By.id("abc"))).sendKeys(Keys.chord(Keys.CONTROL,Keys.ENTER)).build().perform();
     }
 }
